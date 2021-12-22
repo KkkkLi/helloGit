b40fcb477615eaaec1a27bdc22194896429e3ad5
86176 <2902181302@qq.com>
2021-12-22 17:56:43 +0800

    测试提交

---
+++ helllo_git/src/main/java/com/study/HelloGit.java
@@ -0,0 +1,11 @@
+package com.study;
+
+/**
+ * @author quest
+ * @data 2021-12-22 17:49
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
